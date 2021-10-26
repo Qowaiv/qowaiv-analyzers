222771ebb94d832b63d2042c21be96412309edef
Corniel Nobel <mail@corniel.nl>
2021-10-26 12:12:54 +0200

    Make NodeAbstraction implicitly cast-able to SyntaxNode.

--- src/Qowaiv.CodeAnalysis/Rules/ParseShouldNotFail.cs
+++ src/Qowaiv.CodeAnalysis/Rules/ParseShouldNotFail.cs
@@ -2,7 +2,6 @@
 using Microsoft.CodeAnalysis.Diagnostics;
 using Qowaiv.CodeAnalysis.Diagnostics;
 using Qowaiv.CodeAnalysis.Syntax;
-using System;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Reflection;
@@ -21,15 +20,15 @@ namespace Qowaiv.CodeAnalysis.Rules
             context.RegisterSyntaxNodeAction(c =>
             {
                 var invocation = c.Node.InvocationExpression();
-                if (c.Node.Name() == nameof(Parse)
+                if (invocation.Name() == nameof(Parse)
                     && invocation.Arguments.Count() == 1
                     && c.SemanticModel.GetConstantValue(invocation.Expressions.First()).Value is string literal
-                    && c.SemanticModel.GetSymbolInfo(c.Node).Symbol is IMethodSymbol method
+                    && c.SemanticModel.GetSymbolInfo(invocation).Symbol is IMethodSymbol method
                     && method.IsStatic
                     && SymbolEqualityComparer.Default.Equals(method.ContainingType, method.ReturnType)
                     && Parse(method, literal) is { } failure)
                 {
-                    c.ReportDiagnostic(this, c.Node, failure);
+                    c.ReportDiagnostic(this, invocation, failure);
                 }
             },
             SyntaxKinds.Of<TSyntaxKind>().InvocationExpression);
@@ -50,11 +49,5 @@ namespace Qowaiv.CodeAnalysis.Rules
             }
             return null;
         }
-
-        private static bool IsParse(MethodInfo method)
-            => method.Name == nameof(Parse)
-            && method.GetParameters().Length == 1
-            && method.GetParameters()[0].ParameterType == typeof(string)
-            && method.ReturnType == method.DeclaringType;
     }
 }
