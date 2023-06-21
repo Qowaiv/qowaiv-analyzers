9782ea17f572c4375f00137c8a731544c35c3c19
Corniel Nobel <mail@corniel.nl>
2023-06-21 17:29:16 +0200

    boy scout

--- src/Qowaiv.CodeAnalysis.CSharp/Extensions/Microsoft.CodeAnalysis.CodeFixes.CodeFixContext.cs
+++ src/Qowaiv.CodeAnalysis.CSharp/Extensions/Microsoft.CodeAnalysis.CodeFixes.CodeFixContext.cs
@@ -1,22 +1,11 @@
-﻿using Microsoft.CodeAnalysis.CodeActions;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace Microsoft.CodeAnalysis.CodeFixes;
+﻿namespace Microsoft.CodeAnalysis.CodeFixes;
 
 /// <summary>Extensions on <see cref="CodeFixContext"/>.</summary>
 internal static  class CodeFixContextExtensions
 {
     public static async Task<DiagnosticContext?> DiagnosticContext(this CodeFixContext context)
-    {
-        if(context.Diagnostics.FirstOrDefault() is { } diagnostic)
-        {
-            var root =await context.Document.GetSyntaxRootAsync(context.CancellationToken);
-            return new(context.Document, diagnostic, root!);
-        }
-        else
-        {
-            return null;
-        }
-    }
+        => context.Diagnostics.FirstOrDefault() is { } diagnostic
+        && (await context.Document.GetSyntaxRootAsync(context.CancellationToken)) is { } root
+            ? new(context.Document, diagnostic, root)
+            : null;
 }
