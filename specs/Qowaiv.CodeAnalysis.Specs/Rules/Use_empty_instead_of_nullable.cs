90969c1f26626fb9ffa7343ddf9dedd9114a811a
Corniel Nobel <mail@corniel.nl>
2023-06-21 16:25:50 +0200

    Initial setup.

---
+++ specs/Qowaiv.CodeAnalysis.Specs/Rules/Use_empty_instead_of_nullable.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+
+namespace Rules.Use_empty_instead_of_nullable;
+
+public class Verify
+{
+    [Test]
+    public void File_scoped_is_compliant()
+        => new UseEmptyInsteadOfNullable()
+        .ForCS()
+        .AddSource(@"Cases/UseEmptyInsteadOfNullable.cs")
+        .AddReference<Qowaiv.Percentage>()
+        .WithLanguageVersion(LanguageVersion.CSharp10)
+        .Verify();
+}
