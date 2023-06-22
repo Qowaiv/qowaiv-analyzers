fa98cbdef989f45840a9410e14d98a69766ff031
Corniel Nobel <mail@corniel.nl>
2023-06-22 21:29:26 +0200

    Extend rule to deal with this case.

--- src/Qowaiv.CodeAnalysis.CSharp/Rules/DefinePropertiesAsNotNullable.cs
+++ src/Qowaiv.CodeAnalysis.CSharp/Rules/DefinePropertiesAsNotNullable.cs
@@ -1,10 +1,13 @@
-﻿namespace Qowaiv.CodeAnalysis.Rules;
+﻿using System.Reflection;
+
+namespace Qowaiv.CodeAnalysis.Rules;
 
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public sealed class DefinePropertiesAsNotNullable : CodingRule
 {
-    public DefinePropertiesAsNotNullable()
-        : base(Rule.DefinePropertiesAsNotNullable) { }
+    public DefinePropertiesAsNotNullable(): base(
+        Rule.DefinePropertiesAsNotNullable,
+        Rule.DefineEnumPropertiesAsNotNullable) { }
 
     protected override void Register(AnalysisContext context)
     {
@@ -30,17 +33,34 @@ public sealed class DefinePropertiesAsNotNullable : CodingRule
     private static void Report(TypeSyntax syntax, SyntaxNodeAnalysisContext context)
     {
         if (context.SemanticModel.GetTypeInfo(syntax).Type is INamedTypeSymbol type
-            && type.IsValueType
-            && DefaultIsEmpty(type))
+            && NotNullable(type) is { } diagnostic)
         {
-            context.ReportDiagnostic(Rule.DefinePropertiesAsNotNullable, syntax);
+            context.ReportDiagnostic(diagnostic, syntax);
         }
     }
 
-    private static bool DefaultIsEmpty(INamedTypeSymbol type)
-        => type.IsNullableValueType()
-        && type.TypeArguments[0] is INamedTypeSymbol tp
-        && tp.GetMembers("Empty").Any(IsReadOnlyField);
+    private static DiagnosticDescriptor? NotNullable(INamedTypeSymbol type)
+        => type.IsValueType
+        && type.IsNullableValueType()
+        && type.TypeArguments[0] is INamedTypeSymbol inner
+            ? EnumDefaultIsNoneOrEmpty(inner) ?? DefaultIsEmpty(inner)
+            : null;
+
+    private static DiagnosticDescriptor? EnumDefaultIsNoneOrEmpty(INamedTypeSymbol type)
+        => type.TypeKind == TypeKind.Enum
+        && type.GetMembers().Any(IsNoneOrEmpty)
+        ? Rule.DefineEnumPropertiesAsNotNullable : null;
+
+    private static DiagnosticDescriptor? DefaultIsEmpty(INamedTypeSymbol type)
+        => type.GetMembers("Empty").Any(IsReadOnlyField)
+        ? Rule.DefinePropertiesAsNotNullable : null;
+
+    private static bool IsNoneOrEmpty(ISymbol member)
+        => member is IFieldSymbol field
+        && IsZero(field.ConstantValue)
+        && (field.Name.ToUpperInvariant() == "NONE" || field.Name.ToUpperInvariant() == "EMPTY");
+
+    private static bool IsZero(object? value) => Convert.ToInt64(value) == 0;
 
     private static bool IsReadOnlyField(ISymbol member)
         => member is IFieldSymbol field
