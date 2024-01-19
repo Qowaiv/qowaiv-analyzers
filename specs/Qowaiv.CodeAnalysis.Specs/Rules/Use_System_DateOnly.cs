86d7cbbd3e152d125636af82db13a69f0c99d0cd
Corniel Nobel <corniel@gmail.com>
2024-01-19 20:38:34 +0100

    Implement rule.

---
+++ specs/Qowaiv.CodeAnalysis.Specs/Rules/Use_System_DateOnly.cs
@@ -0,0 +1,11 @@
+﻿namespace Rules.Use_System_DateOnly;
+
+public class Verify
+{
+    [Test]
+    public void Rule() => new UseSystemDateOnly()
+        .ForCS()
+        .AddSource(@"Cases/UseSystemDateOnly.cs")
+        .AddReference<Qowaiv.Date>()
+        .Verify();
+}
