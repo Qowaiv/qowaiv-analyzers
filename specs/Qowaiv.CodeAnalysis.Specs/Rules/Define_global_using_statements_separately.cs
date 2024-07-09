c20ac8b01416dea3bdc1054ea03f62d171b23293
Corniel Nobel <corniel@gmail.com>
2024-07-09 13:23:49 +0200

    Define global using statements separately.

---
+++ specs/Qowaiv.CodeAnalysis.Specs/Rules/Define_global_using_statements_separately.cs
@@ -0,0 +1,16 @@
+﻿namespace Rules.Define_global_using_statements_separately;
+
+public class Verify
+{
+    [Test]
+    public void Rule_for_only_globals() => new DefineGlobalUsingStatementsSeparately()
+        .ForCS()
+        .AddSource(@"Cases/DefineGlobalUsingStatementsSeparately.GlobalUsings.cs")
+        .Verify();
+
+    [Test]
+    public void Rule_with_mix() => new DefineGlobalUsingStatementsSeparately()
+        .ForCS()
+        .AddSource(@"Cases/DefineGlobalUsingStatementsSeparately.cs")
+        .Verify();
+}
---
+++ src/Qowaiv.CodeAnalysis.CSharp/Rules/DefineGlobalUsingStatementsSeparately.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Qowaiv.CodeAnalysis.Rules;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class DefineGlobalUsingStatementsSeparately() : CodingRule(Rule.DefineGlobalUsingStatementsSeparately)
+{
+    protected override void Register(AnalysisContext context)
+        => context.RegisterSyntaxNodeAction(Report, SyntaxKind.UsingDirective);
+
+    private void Report(SyntaxNodeAnalysisContext context)
+    {
+        if (IsGlobalDirective(context.Node) && !AllGobal(context.Node))
+        {
+            context.ReportDiagnostic(Diagnostic, context.Node);
+        }
+    }
+
+    private static bool AllGobal(SyntaxNode node)
+        => node.Parent is CompilationUnitSyntax root
+        && root.ChildNodes().All(IsGlobalDirective);
+
+    private static bool IsGlobalDirective(SyntaxNode node)
+        => node is UsingDirectiveSyntax direcive
+        && direcive.ChildTokens().Any(token => token.IsKind(SyntaxKind.GlobalKeyword));
+}
