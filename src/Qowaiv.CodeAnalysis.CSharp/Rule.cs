f31f7368455d9611fa56c3ecc39a697e8c2d0c3e
Laura Kramer <l.kramer@tjip.com>
2024-07-11 14:35:40 +0200

    Remove space, revert message change

--- src/Qowaiv.CodeAnalysis.CSharp/Rule.cs
+++ src/Qowaiv.CodeAnalysis.CSharp/Rule.cs
@@ -50,7 +50,7 @@ public static partial class Rule
     public static DiagnosticDescriptor SealClasses => New(
         id: 0005,
         title: "Seal concrete classes unless designed for inheritance",
-        message: "Seal this {0} or make it explicitly inheritable.",
+        message: "Seal this {0} or make it explicit inheritable.",
         description:
             "Inheritance is one of the pillars of Object Oriented Programming. " +
             "Designing a class to support inheritance however, is hard. As a " +
