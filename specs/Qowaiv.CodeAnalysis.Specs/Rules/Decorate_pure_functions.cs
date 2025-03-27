b8dc41bf9c7096c34d0b0fdafcec81e7af3e801c
Corniel Nobel <corniel@gmail.com>
2025-03-27 10:56:44 +0100

    Readonly modifier is sufficient (#64)

--- specs/Qowaiv.CodeAnalysis.Specs/Rules/Decorate_pure_functions.cs
+++ specs/Qowaiv.CodeAnalysis.Specs/Rules/Decorate_pure_functions.cs
@@ -3,8 +3,7 @@ namespace Rules.Decorate_pure_functions;
 public class Verify
 {
     [Test]
-    public void CSharp()
-        => new DecoratePureFunctions()
+    public void CSharp() => new DecoratePureFunctions()
         .ForCS()
         .AddSource(@"Cases/DecoratePureFunctions.cs")
         .AddReference<FluentAssertions.CustomAssertionAttribute>()
