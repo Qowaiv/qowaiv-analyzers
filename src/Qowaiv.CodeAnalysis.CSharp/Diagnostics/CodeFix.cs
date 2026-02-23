98ab2f943af5b5475691b194352401c0825c7451
Corniel Nobel <corniel@gmail.com>
2026-02-23 10:16:17 +0100

    All code fixes providers come with a FixAllProvider

--- src/Qowaiv.CodeAnalysis.CSharp/Diagnostics/CodeFix.cs
+++ src/Qowaiv.CodeAnalysis.CSharp/Diagnostics/CodeFix.cs
@@ -1,12 +1,15 @@
 namespace Qowaiv.CodeAnalysis.Diagnostics;
 
+/// <summary>Base implementation of a <see cref="CodeFixProvider"/> for the Qowaiv analyzers.</summary>
 public abstract class CodeFix : CodeFixProvider
 {
     protected CodeFix(string diagnosticId) => FixableDiagnosticIds = [diagnosticId];
 
     protected CodeFix(string diagnosticId, params string[] additional) => FixableDiagnosticIds = [diagnosticId, ..additional];
 
+    /// <inheritdoc />
     public sealed override ImmutableArray<string> FixableDiagnosticIds { get; }
 
-    public sealed override FixAllProvider? GetFixAllProvider() => null;
+    /// <inheritdoc />
+    public sealed override FixAllProvider? GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
 }
