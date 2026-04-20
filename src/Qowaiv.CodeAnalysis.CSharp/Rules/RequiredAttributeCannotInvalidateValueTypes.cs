e865359b21ca3c198c32a0667b53aad7ef973642
Corniel Nobel <corniel@gmail.com>
2026-04-20 15:54:16 +0200

    QW0101: [Required] always allowed on Properties/Fields for JSON serializable models (#93)
    * Bump version
    
    * Fixed FP
    
    * Bump version
    
    * Update src/Qowaiv.CodeAnalysis.CSharp/Rules/RequiredAttributeCannotInvalidateValueTypes.cs
    
    Co-authored-by: Sjaaky <sjaaky@delegend.nl>
    
    * Update src/Qowaiv.CodeAnalysis.CSharp/Rules/RequiredAttributeCannotInvalidateValueTypes.cs
    
    Co-authored-by: Sjaaky <sjaaky@delegend.nl>
    
    * Update rules/QW0101.md
    
    Co-authored-by: Sjaaky <sjaaky@delegend.nl>
    
    * ...
    
    ---------
    
    Co-authored-by: Sjaaky <sjaaky@delegend.nl>

--- src/Qowaiv.CodeAnalysis.CSharp/Rules/RequiredAttributeCannotInvalidateValueTypes.cs
+++ src/Qowaiv.CodeAnalysis.CSharp/Rules/RequiredAttributeCannotInvalidateValueTypes.cs
@@ -19,11 +19,25 @@ public sealed class RequiredAttributeCannotInvalidateValueTypes() : CodingRule(R
             foreach (var attribute in member.Attributes)
             {
                 if (attribute.Symbol is { } type
-                    && type.Is(SystemType.System.ComponentModel.DataAnnotations.RequiredAttribute))
+                    && type.Is(SystemType.System.ComponentModel.DataAnnotations.RequiredAttribute)
+                    && !DecoratedWithJsonAttribute(context))
                 {
                     context.ReportDiagnostic(Diagnostic, attribute);
                 }
             }
         }
     }
+
+    private static bool DecoratedWithJsonAttribute(SyntaxNodeAnalysisContext context)
+        => context.Node.Parent?.TryTypeDeclaration(context.SemanticModel) is { } parent
+        && DecoratedWithJsonAttribute(parent.Symbol);
+
+    private static bool DecoratedWithJsonAttribute(INamedTypeSymbol? type)
+        => type?.GetAttributes().Any(IsJsonAttribute) is true
+        || type?.GetProperties().Any(p => p.GetAttributes().Any(IsJsonAttribute)) is true;
+
+    private static bool IsJsonAttribute(AttributeData attr)
+        => attr.AttributeClass?.GetFullMetaDataName() is { Length: > 0 } name
+        && (name.StartsWith("System.Text.Json.Serialization.")
+        || name.StartsWith("Newtonsoft.Json."));
 }
