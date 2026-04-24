51f7f7e693142834b632fd016d3062682e1f7a64
Corniel Nobel <corniel@gmail.com>
2026-04-24 20:58:08 +0200

    Update packages

--- src/Qowaiv.CodeAnalysis.CSharp/Extensions/Microsoft.CodeAnalysis.Diagnostics.SyntaxNodeAnalysisContext.cs
+++ src/Qowaiv.CodeAnalysis.CSharp/Extensions/Microsoft.CodeAnalysis.Diagnostics.SyntaxNodeAnalysisContext.cs
@@ -3,38 +3,30 @@ namespace Microsoft.CodeAnalysis.Diagnostics;
 /// <summary>Extensions on <see cref="SyntaxNodeAnalysisContext"/>.</summary>
 internal static class SyntaxNodeAnalysisContextExtensions
 {
-    /// <summary>Report a <see cref="Diagnostic"/> about a <see cref="SyntaxNode"/>'.</summary>
-    public static void ReportDiagnostic(
-        this SyntaxNodeAnalysisContext context,
-        DiagnosticDescriptor descriptor,
-        SyntaxNode node,
-        params object[] messageArgs)
-        => context.ReportDiagnostic(
-            Diagnostic.Create(
-                descriptor,
-                node.GetLocation(),
-                messageArgs));
+    extension(SyntaxNodeAnalysisContext context)
+    {
+        /// <summary>Report a <see cref="Diagnostic"/> about a <see cref="SyntaxNode"/>'.</summary>
+        public void ReportDiagnostic(DiagnosticDescriptor descriptor, SyntaxNode node, params object[] messageArgs)
+            => context.ReportDiagnostic(
+                Diagnostic.Create(
+                    descriptor,
+                    node.GetLocation(),
+                    messageArgs));
 
-    /// <summary>Report a <see cref="Diagnostic"/> about a <see cref="SyntaxToken"/>'.</summary>
-    public static void ReportDiagnostic(
-        this SyntaxNodeAnalysisContext context,
-        DiagnosticDescriptor descriptor,
-        SyntaxToken token,
-        params object[] messageArgs)
-        => context.ReportDiagnostic(
-            Diagnostic.Create(
-                descriptor,
-                token.GetLocation(),
-                messageArgs));
+        /// <summary>Report a <see cref="Diagnostic"/> about a <see cref="SyntaxToken"/>'.</summary>
+        public void ReportDiagnostic(DiagnosticDescriptor descriptor, SyntaxToken token, params object[] messageArgs)
+            => context.ReportDiagnostic(
+                Diagnostic.Create(
+                    descriptor,
+                    token.GetLocation(),
+                    messageArgs));
 
-    /// <summary>Tries to read the `dotnet_diagnostic.{descriptor.Id}.{property}` option.</summary>
-    public static string? TryGetConfiguredProperty(
-        this SyntaxNodeAnalysisContext context,
-        DiagnosticDescriptor descriptor,
-        string property)
-        => context.Options.AnalyzerConfigOptionsProvider
-            .GetOptions(context.Node.SyntaxTree)
-            .TryGetValue($"dotnet_diagnostic.{descriptor.Id}.{property}", out var value)
-            ? value
-            : null;
+        /// <summary>Tries to read the `dotnet_diagnostic.{descriptor.Id}.{property}` option.</summary>
+        public string? TryGetConfiguredProperty(DiagnosticDescriptor descriptor,string property)
+            => context.Options.AnalyzerConfigOptionsProvider
+                .GetOptions(context.Node.SyntaxTree)
+                .TryGetValue($"dotnet_diagnostic.{descriptor.Id}.{property}", out var value)
+                ? value
+                : null;
+    }
 }
--- src/Qowaiv.CodeAnalysis.CSharp/Extensions/System.String.cs
+++ src/Qowaiv.CodeAnalysis.CSharp/Extensions/System.String.cs
@@ -2,11 +2,12 @@ namespace System;
 
 internal static class StringExtensions
 {
-    /// <summary>Returns true if the string match when casing is ignored.</summary>
-    public static bool Matches(this string str, string other)
-        => str.Equals(other, StringComparison.OrdinalIgnoreCase);
+    extension(string str)
+    {
+        /// <summary>Returns true if the string match when casing is ignored.</summary>
+        public bool Matches(string other) => str.Equals(other, StringComparison.OrdinalIgnoreCase);
 
-    /// <summary>Returns true if the strings match when casing is ignored.</summary>
-    public static bool IsContainedBy(this string str, string other)
-        => other.IndexOf(str, StringComparison.OrdinalIgnoreCase) != -1;
+        /// <summary>Returns true if the strings match when casing is ignored.</summary>
+        public bool IsContainedBy(string other) => other.IndexOf(str, StringComparison.OrdinalIgnoreCase) != -1;
+    }
 }
