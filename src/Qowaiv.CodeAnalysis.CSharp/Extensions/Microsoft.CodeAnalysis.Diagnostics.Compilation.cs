942aa0cd83ec9e6078ccce93f6a75632962a0e08
Corniel Nobel <corniel@gmail.com>
2026-06-02 09:03:15 +0200

    Clean-up CSharp Language Version extension (#103)
    * CSharp Langauage Version clean-up
    
    * Update packages

--- src/Qowaiv.CodeAnalysis.CSharp/Extensions/Microsoft.CodeAnalysis.Diagnostics.Compilation.cs
+++ src/Qowaiv.CodeAnalysis.CSharp/Extensions/Microsoft.CodeAnalysis.Diagnostics.Compilation.cs
@@ -3,6 +3,9 @@ namespace Microsoft.CodeAnalysis.Diagnostics;
 /// <summary>Extensions on <see cref="Compilation"/>.</summary>
 internal static class CompilationExtensions
 {
-    public static LanguageVersion LanguageVersion(this Compilation compilation)
-        => compilation is CSharpCompilation cs ? cs.LanguageVersion : default;
+    extension(Compilation compilation)
+    {
+        public LanguageVersion CSharpVersion
+            => compilation is CSharpCompilation cs ? cs.LanguageVersion : default;
+    }
 }
--- src/Qowaiv.CodeAnalysis.CSharp/LanguageVersionExt.cs
+++
@@ -1,6 +0,0 @@
-namespace Qowaiv.CodeAnalysis;
-
-internal static class LanguageVersionExt
-{
-    public static readonly LanguageVersion CSharp11 = (LanguageVersion)1100;
-}
